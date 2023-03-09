37b57c3fbc451343c9a76668e59e1c2336164659
Praveen Kumar Bhuyan <praveenkumarbhuyan@anoralabs.com>
2023-03-09 11:39:38 +0530

    commit done @main.py on 9th March 2023

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 def func():
-  # For function related addition1
-  # For function related addition2
-  # For function related addition3
-  # For function related addition4
-  # For function related addition5
-  # For function related addition6
-  pass
+    # For function related addition coded changed
+    # For function related addition5 coded changed
+    # For function related addition6 coded changed
+    pass
 
 
-if __name__=="__main__":
-  func()
+def func1():
+    # Code changed in here
+    # Code changed in here
+    # Code changed in here
+    # Code changed in here
+    pass
+
+
+if __name__ == "__main__":
+    func()
+    func1()
