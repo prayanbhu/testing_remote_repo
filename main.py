4534e7ea389f7707dd1fe2bbb561c99c229c7a66
prayanbhu <37943606+prayanbhu@users.noreply.github.com>
2023-03-09 11:35:38 +0530

    Created main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+def func():
+  # For function related addition1
+  # For function related addition2
+  # For function related addition3
+  # For function related addition4
+  # For function related addition5
+  # For function related addition6
+  pass
+
+
+if __name__=="__main__":
+  func()
