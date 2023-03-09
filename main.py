928d388a81261bc66378e3fce995675254a77ec3
Praveen Kumar Bhuyan <praveenkumarbhuyan@anoralabs.com>
2023-03-09 11:43:43 +0530

    extra func3 deleted.

--- main.py
+++ main.py
@@ -4,12 +4,14 @@ def func():
     # For function related addition6 coded changed
     pass
 
+
 def func3():
     # Code changed in here func3
     # Code changed in here func3
     # Code changed in here func3
     # Code changed in here func3
     pass
+
 
 def func1():
     # Code changed in here
@@ -17,14 +19,6 @@ def func1():
     # Code changed in here
     # Code changed in here
     pass
-
-
-def func3():
-  # Code changed in here
-  # Code changed in here
-  # Code changed in here
-  # Code changed in here
-  pass
 
 
 if __name__ == "__main__":
