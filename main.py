67bcb79d4284b56548748f64d495363e5a1fc898
prayanbhu <37943606+prayanbhu@users.noreply.github.com>
2023-03-09 11:41:59 +0530

    func3 introduced.

--- main.py
+++ main.py
@@ -4,6 +4,12 @@ def func():
     # For function related addition6 coded changed
     pass
 
+def func3():
+    # Code changed in here func3
+    # Code changed in here func3
+    # Code changed in here func3
+    # Code changed in here func3
+    pass
 
 def func1():
     # Code changed in here
@@ -16,3 +22,4 @@ def func1():
 if __name__ == "__main__":
     func()
     func1()
+    func3()
